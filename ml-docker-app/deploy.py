2ad0e57e651146ee5ee7750a781cd40a4c5768fc
Ann Silva <annmargaret.silva@gmail.com>
2024-04-09 21:24:38 -0400

    update python file for docker

--- ml-docker-app/deploy.py
+++ ml-docker-app/deploy.py
@@ -4,14 +4,14 @@ import boto3
 role = "arn:aws:iam::072440950856:role/service-role/AmazonSageMaker-ExecutionRole-20240314T135892"
 
 # Define your custom Docker image URI in Amazon ECR
-docker_image_uri = "072440950856.dkr.ecr.us-east-1.amazonaws.com/bankchurnprediction:latest4"
+docker_image_uri = "072440950856.dkr.ecr.us-east-1.amazonaws.com/bankchurnprediction:latest6"
 model_data_url = "s3://sagemaker-us-east-1-072440950856/sagemaker/model_artifacts.tar.gz"
 
 # Create a SageMaker client
 sagemaker_client = boto3.client('sagemaker')
 
 # Define model name and primary container
-model_name = "my-randomforest-model4"
+model_name = "my-randomforest-model6"
 primary_container = {
     'Image': docker_image_uri,
     'ModelDataUrl': model_data_url,  # Location of your model artifact in S3
@@ -25,14 +25,14 @@ sagemaker_client.create_model(
 )
 
 # Define endpoint configuration name
-endpoint_config_name = "my-endpoint-config2"
+endpoint_config_name = "my-endpoint-config6"
 
 # Define instance type and count
 instance_type = "ml.t2.medium"
 instance_count = 1
 
 # Define endpoint name
-endpoint_name = "my-endpoint4"
+endpoint_name = "my-endpoint6"
 
 log_group_name = '/aws/sagemaker/random-forest-logs'
 log_capture_prefix = 's3://sagemaker-us-east-1-072440950856/sagemaker/outputlogs/'
--- ml-docker-app/inference.py
+++ ml-docker-app/inference.py
@@ -10,14 +10,20 @@ app.debug = True
 # Function to load the model
 def model_fn(model_dir):
     # Load the model from the model_dir
-    
-    model = joblib.load(os.path.join(model_dir, "random_forest.pkl"))
+    script_dir = os.path.dirname(model_dir)
+    model_path = os.path.join(script_dir, "random_forest.pkl")
+    model = joblib.load(model_path)
     return model
 
 # Function to perform inference
 def predict_fn(input_data, model):
-    # Convert input data to numpy array
-    input_data_np = np.array(input_data['instances'])
+    # Check if input_data is in JSON format
+    if isinstance(input_data, dict) and 'instances' in input_data:
+        input_data_np = np.array(input_data['instances'])
+    elif isinstance(input_data, list) and isinstance(input_data[0], list):
+        input_data_np = np.array(input_data)
+    else:
+        raise ValueError('Input data format not supported')
     
     # Perform inference using the loaded model
     predictions = model.predict(input_data_np)
@@ -27,10 +33,17 @@ def predict_fn(input_data, model):
 @app.route('/invocations', methods=['POST'])
 def predict():
     # Load the model
-    model = model_fn("/opt/ml/model")
-
-    # Parse input data
-    input_data = request.json
+    model = model_fn(os.path.realpath(__file__))
+    content_type = request.headers.get('Content-Type')
+    
+    # Parse input data based on content type
+    if content_type == 'application/json':
+        input_data = request.json
+    elif content_type == 'text/csv':
+        input_data = request.data.decode('utf-8').split('\n')
+        # Split CSV rows into lists of values
+        input_data = [row.split(',') for row in input_data if row.strip()]
+        input_data = input_data[1:]  # Remove header row from data
 
     # Perform inference
     predictions = predict_fn(input_data, model)
